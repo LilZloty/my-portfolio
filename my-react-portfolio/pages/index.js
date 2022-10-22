b178719f985a09338e9d8c1cace7e2cf27b16a2a
Théo Daudebourg <54123008+LilZloty@users.noreply.github.com>
2022-10-21 20:28:09 -0500

    nez images

--- my-react-portfolio/pages/index.js
+++ my-react-portfolio/pages/index.js
@@ -3,17 +3,17 @@ import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
 import { useState } from "react";
 import Image from "next/image";
-import ImageTheo from "../public/pfp.png";
-import Designicon from "../public/designicon.png";
-import Techstackicon from "../public/techstackicon.png";
-import Cmsicon from "../public/cmsandothericon.png";
-import Services from "../public/services.png";
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
+import ImageTheo from "../public/images/pfp.png";
+import Designicon from "../public/images/designicon.png";
+import Techstackicon from "../public/images/techstackicon.png";
+import Cmsicon from "../public/images/cmsandothericon.png";
+import Services from "../public/images/services.png";
+import sakaspirits from "../public/images/sakaspirits.jpg";
+import sliimbay from "../public/images/sliimbay.png";
+import tinypods from "../public/images/tinypods.png";
+import sweetnabbody from "../public/images/sweetnabbody.png";
+import mayfurs from "../public/images/sweetnabbody.png";
+import anese from "../public/images/sweetnabbody.png";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -59,7 +59,7 @@ export default function Home() {
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 PY-3 text-gray-600 dark:text-gray-300">
-            <a href="https://www.linkedin.com/in/th%C3%A9o-daudebourg-85373a177/?locale=en_US" className="hover:text-[#595CFF]"><AiFillLinkedin/></a>
+            <a href="https://www.linkedin.com/in/th%C3%A9o-daudebourg-85373a177/?locale=en_US" className="hover:text-[#595CFF]"><AiFillLinkedin /></a>
             <a href="https://www.youtube.com/channel/UChH_oneD9OKdCK-ChHFz-2w" className="hover:text-[#595CFF]"><AiFillYoutube /></a>
           </div>
           <div class="flex flex-col justify-center items-center py-10 leading-8">
@@ -68,7 +68,7 @@ export default function Home() {
                 className="animate-pulse bg-gradient-to-r from-[#595CFF] to-[#C6F8FF] text-white px-10 py-2 rounded-md border-none font-nikea"
                 href="#"
               >
-                
+
                 Resume
               </a>
             </button>
@@ -76,15 +76,14 @@ export default function Home() {
         </section>
         <section>
           <div className="pt-10">
-          
-          
+
+
           </div>
         </section>
-        <section className="text-center py-10 ">
+        <section className="text-center ">
           <div className="py-10">
             <hr class="my-4 mx-auto w-48 h-1 bg-gray-100 rounded border-0 md:my-10 dark:bg-gray-700 "></hr>
-            <h2 className="text-3xl text-[#595CFF]">Services I offer</h2>
-            <h3 className="pt-3 ">Why it will not be your brand?!</h3>
+            <h2 className="text-3xl text-[#595CFF]">a brief summary from my experience.</h2> 
             <p className="text-md py-2 leading-8 dark:text-gray-300">
               Since 2017, i <b> design</b> and <b> develop</b>,
               <b> Shopify brands</b>, who are really making <b>sells</b>. <br />
@@ -109,7 +108,7 @@ export default function Home() {
                 Beautiful Designs
               </h2>
               <h3 className="py-2 text-xs md:text-sm">
-               Which tool i use to design your brand
+                Which tool i use to design your brand
               </h3>
               <p className="py-1">Suite Adobe - Photoshop - Illustrator </p>
               <p className="py-1">Blender - Cinema 4D</p>
@@ -133,28 +132,26 @@ export default function Home() {
               <h2 className="text-lg  text-[#595CFF]">
                 CMS PLATFORM
               </h2>
-              <h3 className="py-2 text-xs md:text-sm">
+              <h4 className="py-2 text-xs md:text-sm">
                 Which tool i use to transform my code
-              </h3>
+              </h4>
               <p className="py-1">Shopify and Shopify Plus</p>
               <p className="py-1">Wordpress & WooCommerce</p>
             </div>
           </div>
         </section>
-        <section className="py-10">
-          <div>
+        <section className="py-10 ">
+          <div className="text-center py-20">
             <h3 className="text-3xl py-1 text-[#595CFF] ">Portofolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-300">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
+              Since the beginning of my journey as a front-end web developer and designer.<br></br>
+              I&apos;ve done remote work for agencies consulted for startups
+              and collaborated with talented people to create digital products
               for both business and consumer use.
             </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               I offer from a wide range of services, including brand design,
-              programming and teaching.
+              programming and e-commerce consulting.
             </p>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
@@ -164,7 +161,8 @@ export default function Home() {
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web1}
+                src={sweetnabbody}
+                alt="Sweetnabbody"
               />
             </div>
             <div className="basis-1/3 flex-1">
@@ -173,7 +171,7 @@ export default function Home() {
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web2}
+                src={sakaspirits}
               />
             </div>
             <div className="basis-1/3 flex-1">
@@ -182,7 +180,7 @@ export default function Home() {
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web3}
+                src={sweetnabbody}
               />
             </div>
             <div className="basis-1/3 flex-1">
@@ -191,7 +189,7 @@ export default function Home() {
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web4}
+                src={sweetnabbody}
               />
             </div>
             <div className="basis-1/3 flex-1">
@@ -200,7 +198,7 @@ export default function Home() {
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web5}
+                src={tinypods}
               />
             </div>
             <div className="basis-1/3 flex-1">
@@ -209,7 +207,7 @@ export default function Home() {
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web6}
+                src={sliimbay}
               />
             </div>
           </div>
