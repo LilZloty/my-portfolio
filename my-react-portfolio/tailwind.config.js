f8910fe7987b85cc68abf5e6d1efbf4a6571f0da
Théo Daudebourg <54123008+LilZloty@users.noreply.github.com>
2023-03-15 16:11:08 -0600

    font

--- my-react-portfolio/tailwind.config.js
+++ my-react-portfolio/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       fontFamily: {
       sans:['Inter','sans-serif'],
       alka:['Alkami','serif'],
-      nikea:['Nikea-Regular'],
+      nikea:['NIKEA', 'sans-serif'],
       fira:['Fira Sans', 'sans-serif'],
       Inter:['Inter', 'sans-serif'],
     },
