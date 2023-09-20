0c06d3f1617982e7f6a44d3141541c5c8f322b0f
Théo Daudebourg <54123008+LilZloty@users.noreply.github.com>
2023-09-19 20:04:06 -0600

    Push

--- portfolio-theo/pages/index.js
+++ portfolio-theo/pages/index.js
@@ -19,12 +19,12 @@ export default function Home() {
       <main className="px-10 md:px-20 lg:px-40 dark:bg-[#121212f0] dark:text-white py-10">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-black dark:text-[#4262b1]">Theo Daudebourg</h1>
+            <h1 className="text-[#202223] dark:text-[#4262b1]">Theo Daudebourg</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
                   onClick={() => setDarkMode(!darkMode)}
-                  className="cursor-pointer text-2xl text-black dark:text-white"
+                  className="cursor-pointer text-2xl text-[#202223] dark:text-white"
                 />
               </li>
             </ul>
@@ -48,20 +48,20 @@ export default function Home() {
             <h2 className="text-2xl py-2 text-[#595CFF] md:text-4xl">
               Theo Daudebourg
             </h2>
-            <h3 className="py-2 md:text-1xl hover:animate-bounce text-black">Front End Web Developer</h3>
-            <p className="py-5 text-black dark:text-gray-300 ">
+            <h3 className="py-2 md:text-1xl hover:animate-bounce text-[#202223]">Front End Web Developer</h3>
+            <p className="py-5 text-[#202223] dark:text-gray-300 ">
               Everyday i develop and design unique brands on
               <b> Shopify</b> & <b>Wordpress</b>.
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 PY-3 text-gray-600 dark:text-gray-300">
-            <a href="https://www.linkedin.com/in/theodaudebourg" className="hover:text-[#595CFF] text-black"><AiFillLinkedin /></a>
-            <a href="https://www.youtube.com/channel/UChH_oneD9OKdCK-ChHFz-2w" className="hover:text-[#595CFF] text-black "><AiFillYoutube /></a>
+            <a href="https://www.linkedin.com/in/theodaudebourg" className="hover:text-[#595CFF] text-[#202223]"><AiFillLinkedin /></a>
+            <a href="https://www.youtube.com/channel/UChH_oneD9OKdCK-ChHFz-2w" className="hover:text-[#595CFF] text-[#202223] "><AiFillYoutube /></a>
           </div>
           <div className="flex flex-col justify-center items-center py-10 leading-8">
             <button className="">
               <a
-                className="animate-pulse bg-gradient-to-r from-[#595CFF] to-[#C6F8FF] text-black px-10 py-2 rounded-md border-none font-nikea shadow-md" 
+                className="animate-pulse bg-gradient-to-r from-[#595CFF] to-[#C6F8FF] text-[#202223] px-10 py-2 rounded-md border-none font-nikea shadow-md" 
                 href="#"
               >
 
@@ -69,12 +69,13 @@ export default function Home() {
               </a>
             </button>
           </div>
+        
         </section>
         <section className="text-center">
           <div className="py-10">
             <hr className="my-4 mx-auto w-48 h-1 bg-gray-100 rounded border-0 md:my-10 dark:bg-gray-700 "></hr>
             <h2 className="text-3xl text-[#595CFF] pb-5 uppercase">A Brief summary <br></br>of my experience</h2>
-            <p className="my-4 text-black mx-auto max-w-2xl"> Since 2017, I've been designing and developing Shopify brands that sell. I have worked and continue to work with 6-7 figure brands including the top 100 brands on Shopify, Shopify Plus, and Wordpress. From the development and design of a theme from scratch up to smaller features.
+            <p className="my-4 text-[#202223] mx-auto max-w-2xl"> Since 2017, I've been designing and developing Shopify brands that sell. I have worked and continue to work with 6-7 figure brands including the top 100 brands on Shopify, Shopify Plus, and Wordpress. From the development and design of a theme from scratch up to smaller features.
 Or in the creation of an entire brand identity. I was engaged and had to wear multiple hats throughout my career.
             </p>
           </div>
@@ -82,63 +83,83 @@ Or in the creation of an entire brand identity. I was engaged and had to wear mu
         <section className="grid h-auto place-items-center w-auto">
           <div className="lg:flex gap-10">
             
-            <div className="glowing text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white dark:bg-opacity-5 dark:shadow-none dark:text-gray-300  barbelssides-bg">
-              <Image
-                alt="WebDesignicon"
-                src="/images/designicon.png"
-                width={100}
-                height={100}
-                 />
-              <h2 className="text-lg text-[#595CFF]">
-                Beautiful Designs
-              </h2>
-              <h3 className="py-2 text-xs md:text-sm">
-                Which tool i use to design your brand
-              </h3>
-              <p className="py-1">Suite Adobe - Photoshop - Illustrator </p>
-              <p className="py-1">Blender - Cinema 4D</p>
-              <p className="py-1"> Figma </p>
-              </div>
               
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white dark:bg-opacity-5 dark:shadow-none dark:text-gray-300">
-              <Image
-                alt="Technologieicon"
-                src="/images/techstackicon.png"
-                width={100}
-                height={100}
-                 />
-              <h2 className="text-lg text-[#595CFF]">Tech Stack</h2>
-              <h3 className="py-2 text-xs md:text-sm">
-                What i use to code the desire interface
-              </h3>
-              <p className="py-1">HTML - CSS/SCSS - Tailwind CSS</p>
-              <p className="py-1">Javascript - React JS - Next JS</p>
-              <p className="py-1"> JSON - JQUERY - PHP </p>
-              <p className="py-1">
-                Node JS & MySQL, Liquid
-              </p>
-            </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white dark:bg-opacity-5 dark:shadow-none dark:text-gray-300">
-              <Image
-                alt="Cmsicon"
-                src="/images/cmsicon.png"
-                width={100}
-                height={100}
+          <div className="max-w-md bg-white rounded-lg border border-gray-300 shadow-lg overflow-hidden transition duration-100 transform hover:bg-gray-100 hover:shadow-md docs-card">
+                <div className="docs-card__body p-12 flex items-center">
+                  <div className="docs-icon docs-icon--32 docs-icon--pickaxe"></div>
+                  <div className="docs-card__content ml-4 flex flex-col items-center text-center">
+                    <Image
+                      alt="WebDesignicon"
+                      src="/images/desktopicon.png"
+                      width={120}
+                      height={120}
+                      className="mx-auto"
+                    />
+                    <h2 className="text-lg text-[#595CFF] mt-2">
+                      Beautiful Designs
+                    </h2>
+                    <h3 className="py-2 text-xs md:text-sm text-[#202223]">
+                      Which tools I use to design your brand
+                    </h3>
+                    <p className="py-1">Suite Adobe - Photoshop - Illustrator</p>
+                    <p className="py-1">Blender - Cinema 4D</p>
+                    <p className="py-1">Figma</p>
+                  </div>
+                </div>
+              </div>
+
+
+              
+              <div className="max-w-md bg-white rounded-lg border border-gray-300 shadow-lg overflow-hidden transition duration-100 transform hover:bg-gray-100 hover:shadow-md docs-card">
+  <div className="docs-card__body p-12 flex items-center">
+    <div className="docs-icon docs-icon--32 docs-icon--pickaxe"></div>
+    <div className="docs-card__content ml-4 flex flex-col items-center text-center">
+      <Image
+        alt="Technologieicon"
+        src="/images/techstackiconns.png"
+        width={120}
+        height={120}
+        className="mx-auto"
+      />
+      <h2 className="text-lg text-[#595CFF] mt-2">Tech Stack</h2>
+      <h3 className="py-2 text-xs md:text-sm text-[#202223]">
+        What I use to code the desired interface
+      </h3>
+      <p className="py-1">HTML - CSS/SCSS - Tailwind CSS</p>
+      <p className="py-1">Javascript - React JS - Next JS</p>
+      <p className="py-1">JSON - jQuery - PHP</p>
+      <p className="py-1">Node JS & MySQL, Liquid</p>
+    </div>
+  </div>
+</div>
+
+          <div className="max-w-md bg-white rounded-lg border border-gray-300 shadow-lg overflow-hidden transition duration-100 transform hover:bg-gray-100 hover:shadow-md docs-card">
+            <div className="docs-card__body p-12 flex items-center">
+              <div className="docs-icon docs-icon--32 docs-icon--pickaxe"></div>
+              <div className="docs-card__content ml-4 flex flex-col items-center text-center">
+                <Image
+                  alt="Cmsicon"
+                  src="/images/cmsicons.png"
+                  width={120}
+                  height={120}
+                  className="mx-auto"
                 />
-              <h2 className="text-lg  text-[#595CFF]">
-                CMS PLATFORM
-              </h2>
-              <h3 className="py-2 text-xs md:text-sm">
-                Which tool i use to transform my code
-              </h3>
-              <p className="py-1">Shopify and Shopify Plus</p>
-              <p className="py-1">Wordpress & WooCommerce</p>
+                <h2 className="text-lg text-[#595CFF] mt-2">CMS PLATFORM</h2>
+                <h3 className="py-2 text-xs md:text-sm text-[#202223]">
+                  Which tool I use to transform my code
+                </h3>
+                <p className="py-1">Shopify and Shopify Plus</p>
+                <p className="py-1">Wordpress & WooCommerce</p>
+              </div>
             </div>
           </div>
+          </div>
+
+          
         </section>
         <section>
           <div className="text-center py-20">
-            <h3 className="text-3xl py-1 text-[#595CFF] ">Portofolio</h3>
+            <h3 className="text-3xl py-1 text-[#595CFF] ">Portfolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-300">
               Since the beginning of my journey as a front-end web developer and designer.<br></br>
               I&apos;ve done remote work for agencies consulted for startups
@@ -167,7 +188,7 @@ Or in the creation of an entire brand identity. I was engaged and had to wear mu
                         <h2 className="text-[#595CFF] dark:[#595CFF]">
                           <a href="https://sweetnabbody.com/">Sweetnabbody</a>
                         </h2>
-                        <p className="text-black dark:text-white">
+                        <p className="text-[#202223] dark:text-white">
                           A Shopify cosmetic brand <br></br>Designed and developed from
                           scratch
                         </p>
@@ -176,11 +197,11 @@ Or in the creation of an entire brand identity. I was engaged and had to wear mu
                             <h3 className="text-[#595CFF] dark:[#595CFF]">
                               Theme Development stack
                             </h3>
-                            <p className="text-black">Liquid, Ajax, Javascript, Shopify </p>
+                            <p className="text-[#202223]">Liquid, Ajax, Javascript, Shopify </p>
                           </li>
                           <li className="pt-2 text-[#595CFF] dark:[#595CFF]">
                             <h3>Web Design</h3>
-                            <p className="text-black">
+                            <p className="text-[#202223]">
                               Photoshop, Illustrator, Figma, Blender{" "}
                             </p>
                           </li>
@@ -247,13 +268,13 @@ Or in the creation of an entire brand identity. I was engaged and had to wear mu
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
               <a href="https://flowbite.com/" className="flex items-center">
-                  <span className="self-center text-2xl font-nikea whitespace-nowrap text-black dark:text-white">Theo Daudebourg</span>
+                  <span className="self-center text-2xl font-nikea whitespace-nowrap text-[#202223] dark:text-white">Theo Daudebourg</span>
               </a>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2">
               <div>
-                  <h2 className="mb-6 text-sm font-semibold text-black uppercase dark:text-white">Follow me</h2>
-                  <ul className="text-black dark:text-gray-400">
+                  <h2 className="mb-6 text-sm font-semibold text-[#202223] uppercase dark:text-white">Follow me</h2>
+                  <ul className="text-[#202223] dark:text-gray-400">
                       <li className="mb-4">
                           <a href="https://github.com/LilZloty" className="hover:underline ">Github</a>
                       </li>
@@ -263,8 +284,8 @@ Or in the creation of an entire brand identity. I was engaged and had to wear mu
                   </ul>
               </div>
               <div>
-                  <h2 className="mb-6 text-sm font-semibold text-black uppercase dark:text-white">Legal</h2>
-                  <ul className="text-black dark:text-gray-400">
+                  <h2 className="mb-6 text-sm font-semibold text-[#202223] uppercase dark:text-white">Legal</h2>
+                  <ul className="text-[#202223] dark:text-gray-400">
                       <li className="mb-4">
                           <a href="#" className="hover:underline">Privacy Policy</a>
                       </li>
@@ -277,14 +298,14 @@ Or in the creation of an entire brand identity. I was engaged and had to wear mu
       </div>
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-black sm:text-center dark:text-gray-400">© 2023 <a href="https://theodaudebourg.vercel.app" className="hover:underline">Theo Daudebourg</a>. All Rights Reserved.
+          <span className="text-sm text-[#202223] sm:text-center dark:text-gray-400">© 2023 <a href="https://theodaudebourg.vercel.app" className="hover:underline">Theo Daudebourg</a>. All Rights Reserved.
           </span>
           <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-              <a href="https://github.com/LilZloty" className="text-black hover:text-gray-900 dark:hover:text-white">
+              <a href="https://github.com/LilZloty" className="text-[#202223] hover:text-gray-900 dark:hover:text-white">
                   <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" /></svg>
                   <span className="sr-only">GitHub account</span>
               </a>
-              <a href="https://www.linkedin.com/in/theodaudebourg" className="hover:text-[#595CFF] text-black">
+              <a href="https://www.linkedin.com/in/theodaudebourg" className="hover:text-[#595CFF] text-[#202223]">
                 <AiFillLinkedin className='w-5 h-5' />
                 </a>
           </div>
