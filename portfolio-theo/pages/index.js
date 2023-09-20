e2cfbf6e0f417278d180039d196e0007c5b1725a
Théo Daudebourg <54123008+LilZloty@users.noreply.github.com>
2023-09-19 18:37:02 -0600

    Push

--- portfolio-theo/pages/index.js
+++ portfolio-theo/pages/index.js
@@ -3,17 +3,6 @@ import Image from "next/image";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
 import { useState, useEffect } from "react";
-import ImageTheo from "../public/images/pfp.png";
-import Designicon from "../public/images/designicon.png";
-import Techstackicon from "../public/images/techstackicon.png";
-import Cmsicon from "../public/images/cmsandothericon.png";
-import Services from "../public/images/services.png";
-import sakaspirits from "../public/images/sakaspirits.jpg";
-import sliimbay from "../public/images/sliimbay.png";
-import tinypods from "../public/images/tinypods.png";
-import sweetnabbody from "../public/images/sweetnabbody.png";
-import mayfurs from "../public/images/mayfursbanner.png";
-import anese from "../public/images/sweetnabbody.png";
 import AnimatedText from './AnimatedText'
 
 export default function Home() {
@@ -47,12 +36,10 @@ export default function Home() {
          
             <Image
               className=""
-              src={ImageTheo}
+              src="/images/pfp.png"
               alt="profilimgtheo"
-              style={{
-                maxWidth: "100%",
-                height: "auto"
-              }} />
+              width={500}
+              height={500}/>
             </div>
             
             </div>
@@ -98,13 +85,10 @@ Or in the creation of an entire brand identity. I was engaged and had to wear mu
             <div className="glowing text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white dark:bg-opacity-5 dark:shadow-none dark:text-gray-300  barbelssides-bg">
               <Image
                 alt="WebDesignicon"
-                src={Designicon}
+                src="/images/designicon.png"
                 width={100}
                 height={100}
-                style={{
-                  maxWidth: "100%",
-                  height: "auto"
-                }} />
+                 />
               <h2 className="text-lg text-[#595CFF]">
                 Beautiful Designs
               </h2>
@@ -119,13 +103,10 @@ Or in the creation of an entire brand identity. I was engaged and had to wear mu
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white dark:bg-opacity-5 dark:shadow-none dark:text-gray-300">
               <Image
                 alt="Technologieicon"
-                src={Techstackicon}
+                src="/images/techstackicon.png"
                 width={100}
                 height={100}
-                style={{
-                  maxWidth: "100%",
-                  height: "auto"
-                }} />
+                 />
               <h2 className="text-lg text-[#595CFF]">Tech Stack</h2>
               <h3 className="py-2 text-xs md:text-sm">
                 What i use to code the desire interface
@@ -140,13 +121,10 @@ Or in the creation of an entire brand identity. I was engaged and had to wear mu
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white dark:bg-opacity-5 dark:shadow-none dark:text-gray-300">
               <Image
                 alt="Cmsicon"
-                src={Cmsicon}
+                src="/images/cmsicon.png"
                 width={100}
                 height={100}
-                style={{
-                  maxWidth: "100%",
-                  height: "auto"
-                }} />
+                />
               <h2 className="text-lg  text-[#595CFF]">
                 CMS PLATFORM
               </h2>
@@ -172,119 +150,94 @@ Or in the creation of an entire brand identity. I was engaged and had to wear mu
               programming and e-commerce consulting.
             </p>
           </div>
-
+          </section>
+          <section>
           <div className="flex flex-col gap-10 lg:flex-row lg:flex-wrap">
           <div className="basis-1/3 flex-1">
-  <div className="container relative w-full">
-    <Image
-      className="rounded-lg object-cover shadow-lg transition-all duration-500 hover:opacity-40"
-      width={"100%"}
-      height={"100%"}
-      src={sweetnabbody}
-      alt="Sweetnabbody"
-      sizes="100vw"
-      style={{
-        width: "100%",
-        height: "auto",
-      }}
-    />
-    <div className="absolute inset-x-0 top-0 bottom-0 flex items-center justify-center">
-      <div className="text-center p-4 bg-transparent transition-all duration-500 opacity-0 hover:opacity-100 hover:bg-[#000000eb] hover:dark:bg-[#212121] rounded-lg md:w-full md:h-full md:rounded-lg sm:h-screen flex flex-col items-center justify-center">
-        <h2 className="text-[#595CFF] dark:[#595CFF]">
-          <a href="https://sweetnabbody.com/">Sweetnabbody</a>
-        </h2>
-        <p className="text-black dark:text-white">
-          A Shopify cosmetic brand <br></br>Designed and developed from
-          scratch
-        </p>
-        <ul className="pt-5 ">
-          <li>
-            <h3 className="text-[#595CFF] dark:[#595CFF]">
-              Theme Development stack
-            </h3>
-            <p className="text-black">Liquid, Ajax, Javascript, Shopify </p>
-          </li>
-          <li className="pt-2 text-[#595CFF] dark:[#595CFF]">
-            <h3>Web Design</h3>
-            <p className="text-black">
-              Photoshop, Illustrator, Figma, Blender{" "}
-            </p>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
+                          <div className="container relative w-full">
+                    <Image
+                      className="rounded-lg object-cover shadow-lg transition-all duration-500 hover:opacity-40"
+                      width={300}
+                                height={300}
+                      src="/images/Sweetnabbody.png"
+                      alt="Sweetnabbody"
+                    />
+                    <div className="absolute inset-x-0 top-0 bottom-0 flex items-center justify-center">
+                      <div className="text-center p-4 bg-transparent transition-all duration-500 opacity-0 hover:opacity-100 hover:bg-[#000000eb] hover:dark:bg-[#212121] rounded-lg md:w-full md:h-full md:rounded-lg sm:h-screen flex flex-col items-center justify-center">
+                        <h2 className="text-[#595CFF] dark:[#595CFF]">
+                          <a href="https://sweetnabbody.com/">Sweetnabbody</a>
+                        </h2>
+                        <p className="text-black dark:text-white">
+                          A Shopify cosmetic brand <br></br>Designed and developed from
+                          scratch
+                        </p>
+                        <ul className="pt-5 ">
+                          <li>
+                            <h3 className="text-[#595CFF] dark:[#595CFF]">
+                              Theme Development stack
+                            </h3>
+                            <p className="text-black">Liquid, Ajax, Javascript, Shopify </p>
+                          </li>
+                          <li className="pt-2 text-[#595CFF] dark:[#595CFF]">
+                            <h3>Web Design</h3>
+                            <p className="text-black">
+                              Photoshop, Illustrator, Figma, Blender{" "}
+                            </p>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
 
 
 
 
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                src={sakaspirits}
-                alt='sakaspirits'
-                sizes="100vw"
-                style={{
-                  width: "100%",
-                  height: "auto"
-                }} />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                src={sweetnabbody}
-                alt='sweetnabbody'
-                sizes="100vw"
-                style={{
-                  width: "100%",
-                  height: "auto"
-                }} />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                src={mayfurs}
-                alt='mayfurs'
-                sizes="100vw"
-                style={{
-                  width: "100%",
-                  height: "auto"
-                }} />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                src={tinypods}
-                alt='tinypods'
-                sizes="100vw"
-                style={{
-                  width: "100%",
-                  height: "auto"
-                }} />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                src={sliimbay}
-                alt='sliimbay'
-                sizes="100vw"
-                style={{
-                  width: "100%",
-                  height: "auto"
-                }} />
-            </div>
-          </div>
+                            <div className="basis-1/3 flex-1">
+                              <Image
+                                className="rounded-lg object-cover"
+                                width={300}
+                                height={300}
+                                alt="sakaspirits"
+                                src="/images/sakaspirits.jpg"
+                              
+                      />
+                            </div>
+                            <div className="basis-1/3 flex-1">
+                              <Image
+                                className="rounded-lg object-cover"
+                                width={300}
+                                height={300}
+                                src="/images/Sweetnabbody.png"
+                                alt="Sweetnabbody" />
+                            </div>
+                            <div className="basis-1/3 flex-1">
+                              <Image
+                                className="rounded-lg object-cover"
+                                width={300}
+                                height={300}
+                                src="/images/mayfursbanner.png"
+                                alt="mayfurs"/>
+                            </div>
+                            <div className="basis-1/3 flex-1">
+                              <Image
+                                className="rounded-lg object-cover"
+                                width={300}
+                                height={300}
+                                src="/images/Sweetnabbody.png"
+                                alt="Sweetnabbody"
+                              />
+                            </div>
+                            <div className="basis-1/3 flex-1">
+                              <Image
+                                className="rounded-lg object-cover"
+                                width={300}
+                                height={300}
+                                src="/images/sliimbay.png"
+                                alt="Sliimbay" 
+                                />
+                            </div>
+                          </div>
         </section>
 
       </main>
@@ -328,7 +281,7 @@ Or in the creation of an entire brand identity. I was engaged and had to wear mu
           </span>
           <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
               <a href="https://github.com/LilZloty" className="text-black hover:text-gray-900 dark:hover:text-white">
-                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" /></svg>
+                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" /></svg>
                   <span className="sr-only">GitHub account</span>
               </a>
               <a href="https://www.linkedin.com/in/theodaudebourg" className="hover:text-[#595CFF] text-black">
