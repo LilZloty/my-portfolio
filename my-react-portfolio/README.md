7cf7f4a1feea2ea3617efd813b80d323f44a82ad
Théo Daudebourg <54123008+LilZloty@users.noreply.github.com>
2024-07-02 18:04:00 -0600

    Delete my-react-portfolio directory

--- my-react-portfolio/README.md
+++
@@ -1,4 +0,0 @@
-!! Still in production !!
-
-Theo Daudebourg portfolio made with React JS, Next JS and Tailwind CSS. <br>
-https://theodaudebourg.vercel.app/
--- my-react-portfolio/pages/colors.js
+++
@@ -1,5 +0,0 @@
-export const PRIMARY_COLOR = "#595CFF";
-export const DARK_BG_COLOR = "#121212";
-export const TEXT_WHITE_COLOR = "#FFFFFF";
-export const TEXT_GRAY_COLOR = "#808080";
-export const TEXT_DARK_GRAY_COLOR = "#4A4A4A";
